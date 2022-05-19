75ad812258d391ba24738b27cc60c0d9e797d463
Mrinal Das <mrinal.das@fexle.com>
2022-05-19 15:23:58 +0530

    feature_commit2

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 public class Sum {
     public static void main(String[] args) {
         int a = 5;
-        int b = 6;
+        int b = 4;
         System.out.println("The Sum is: " + (a + b));
     }
 }
